d7d2596f0473ec50e5c8720404be4b52c2bf35e4
Thainá Ferreira <thaina.silva254@etec.sp.gov.br>
2022-06-01 19:54:24 -0300

    projeto feito por diversão

---
+++ calculator.js
@@ -0,0 +1,23 @@
+function insert(number)
+{
+    var num = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = num + number;
+}
+function clean()
+{
+    document.getElementById('resultado').innerHTML = "";
+}
+function back()
+{
+    var resultado = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1);
+}
+function calcular()
+{
+    var resultado = document.getElementById('resultado').innerHTML;
+    if (resultado)
+    {
+        document.getElementById('resultado').innerHTML = eval(resultado);
+        /* a função eval executará as operações */
+    }
+}
